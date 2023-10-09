0c5ca9a73192fe7745af58bd5752fd8f9ffef472
Prasadini V.G.H <prasadinivgh.21@uom.lk>
2023-10-09 19:38:13 +0530

    topbar
    create top bar and styling

---
+++ blog_app/src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './home.css'
+import Header from '../../head/Header'
+
+export default function Home() {
+  return (
+    <div className='home'>
+        <Header/>
+        <p>homepage</p>
+    </div>
+  )
+}
---
+++ blog_app/src/topbar/TopBar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./topbar.css";
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import SearchIcon from '@mui/icons-material/Search';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
+export default function TopBar() {
+  return (
+    <div className="top">
+      <div className="topLeft">
+            <FacebookIcon className="topIcon"/>
+            <TwitterIcon className="topIcon"/>
+            <PinterestIcon className="topIcon"/>
+            <InstagramIcon className="topIcon"/>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">WRITE</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        <img src="../../images/me.jpg" alt="" className="topImage" />
+        <SearchIcon className="topSearchIcon"/>
+      </div>
+    </div>
+  );
+}
