7beb3260d9f1d456c007a0ebfead0febdf7c2027
Prasadini V.G.H <prasadinivgh.21@uom.lk>
2023-10-10 00:58:34 +0530

    singlepost
    complete single page

--- blog_app/src/components/singlePost/SinglePost.js
+++ blog_app/src/components/singlePost/SinglePost.js
@@ -1,8 +1,62 @@
 import React from 'react'
 import './singlepost.css'
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function SinglePost() {
   return (
-    <div className='singlePost'>SinglePost</div>
+    <div className='singlePost'>
+        <div className="singlePostWrapper">
+            <img src="../../images/2.jpg" alt="" className="singlePostImg" />
+            <h1 className="singlePostTitle">
+                Lorem ipsum dolor sit amet
+                <div className="singlePostEdit">
+                    <EditIcon className='singlePostIcon'/>
+                    <DeleteIcon className='singlePostIcon'/>
+                </div>
+            </h1>
+            <div className="singlePostInfo">
+                <span className="singlePostAuthor">
+                    Author: <b>Prasadini</b>
+                </span>
+                <span className="singlePostDate">1 hour ago</span>
+            </div>
+            <p className="singlePostDes">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nihil quisquam iste mollitia deleniti beatae et corporis alias repellendus quidem aperiam hic maxime maiores ea ab quibusdam eveniet, labore ut?
+            </p>
+        </div>
+    </div>
   )
 }
