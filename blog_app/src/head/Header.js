05aa9541e747a33d57026d948c14ff337ccefed7
Prasadini V.G.H <prasadinivgh.21@uom.lk>
2023-10-09 20:19:56 +0530

    header
    complete header

--- blog_app/src/head/Header.js
+++ blog_app/src/head/Header.js
@@ -4,7 +4,11 @@ import './header.css'
 export default function Header() {
   return (
     <div className='header'>
-        header
+        <div className="headerTitle">
+            <span className="headerTitlesm">React & Node</span>
+            <span className="headerTitlelg">Blog</span>
+        </div>
+        <img src="../../images/3.jpg" alt="" className="headerImage" />
     </div>
   )
 }
